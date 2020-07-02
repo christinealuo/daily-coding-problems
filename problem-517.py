7de755b9d9cd0dca112f3781d0132fecb8fe9fe7
Christine Luo <christine_luo@intuit.com>
2020-07-02 12:59:48 -0700

    July 2 daily coding challenge: couldn't figure it out

---
+++ problem-517.py
@@ -0,0 +1,7 @@
+# 3 -> 7 -> 8 -> 10
+# 99 -> 1 -> 8 -> 10
+# 2 -> 8 -> 10
+# Result: 8
+
+# If we assume that the linked lists are the same length, then this is easy peasy
+# Traverse one by one together until you reach a node that is equal
