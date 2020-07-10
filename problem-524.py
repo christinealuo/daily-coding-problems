684bbc26b2ec20cf80e6f1beb9a25caad38825b7
Christine <christineluo@berkeley.edu>
2020-07-09 23:24:14 -0700

    July 9 daily coding problem complete

---
+++ problem-524.py
@@ -0,0 +1,19 @@
+def problem_524(numbers):
+    sumSoFar = 0  # 42
+    startingIndex = 0
+    for index, number in enumerate(numbers):
+        if sumSoFar == 0 and number > 0:
+            sumSoFar = number
+            startingIndex = 0
+        elif sumSoFar != 0 and sumSoFar + number > 0:
+            sumSoFar += number
+        elif sumSoFar != 0 and sumSoFar + number < 0:
+            sumSoFar = 0
+            startingIndex = 0
+    return sumSoFar
+
+
+numbers = [34, -50, 42, 14, -5, 86]
+print(problem_524(numbers))
+numbers2 = [-5, -1, -8, -9]
+print(problem_524(numbers2))
